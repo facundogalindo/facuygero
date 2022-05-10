982761a6272429eb15e980fd8d0b03b0d384c7cd
facundogalindo <galindofacundo@gmail.com>
2022-05-10 09:44:34 -0300

    mi primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+ventana = tk.Tk()
+ventana.geometry('600x400')
+ventana.title('Hola mundo')
+
+ventana.iconbitmap('icono.ico')
+ventana.config(background='#CA57C8')
+boton1 = ttk.Button(ventana, text='Hola')
+boton1.pack()
+#ultimo metodo
+ventana.mainloop()
