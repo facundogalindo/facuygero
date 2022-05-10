1ce214401840784a265b23b6b533c67505290bcf
GeroDLuffy <gerox2009@hotmail.com>
2022-05-10 10:39:39 -0300

    Esto lo hizo el kpo de Gerox

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ ventana.title('Hola mundo')
 
 ventana.iconbitmap('icono.ico')
 ventana.config(background='#CA57C8')
-boton1 = ttk.Button(ventana, text='Hola')
+boton1 = ttk.Button(ventana, text='Puto el que lee')
 boton1.pack()
 #ultimo metodo
 ventana.mainloop()
